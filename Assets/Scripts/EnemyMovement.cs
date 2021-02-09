3d0b8a2c9fe61defc6701cd88d3b19e768d1303e
Alyssa Montgomery <alyssamontgomery99@gmail.com>
2021-02-08 20:59:25 -0700

    Added mouse prefab

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private float turnSpeed = 200.0f;
+    public float speed = 2.0f;
+    public float horizontalInput = 1.0f;
+    public float forwardInput = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
+    }
+}
