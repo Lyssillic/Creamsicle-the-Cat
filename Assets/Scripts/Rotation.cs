f0173deb501d1ed4e52b988ddbafac6c133e7759
Alyssa Montgomery <alyssamontgomery99@gmail.com>
2021-02-14 21:21:35 -0700

    Updated folders to be cleaner, added popsicle and gems.

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+    public Vector3 rotationAngle = new Vector3(0, 0, 10);
+    public float rotationSpeed = 15;
+
+	void Update () {
+        transform.Rotate(rotationAngle * rotationSpeed * Time.deltaTime);
+	}
+}
